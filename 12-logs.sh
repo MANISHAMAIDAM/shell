e3e374e452303eb086103e31b4dad80d85bf069f
manishamaidam <maidammanisha.gmail.com>
2025-03-22 22:44:03 +0530

    logging outputs

---
+++ 12-logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIME_STAMP=$($date +%F-%H-%M-%S)
+SCRPIT=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT-$TIME_STAMP.log
+
+VALIDATE()
+{
+   if [ $1 -ne 0 ]
+   then 
+     echo  " $2 FAILED"
+     exit 1
+    else
+      echo " $2 SUCCESS"
+    fi 
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+
+dnf install mysql -y &>>LOG_FILE
+VALIDATE $? "Installation of mysql"
+
+dnf install git -y &>>LOG_FILE
+VALIDATE $? "Installation of GIT"
+
