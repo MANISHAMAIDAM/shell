c034bf5f6ed1e109801a5edda27ba4c1d3c3093c
manishamaidam <maidammanisha.gmail.com>
2025-03-22 13:00:43 +0530

    greetings

---
+++ 01-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Shiv:: Hello Parvathi, How are you?"
+echo "Parvati:: Hi Shiv, I am fine. How are you?"
+echo "Shiv: I am fine too. how is your work?"
+echo "Parvathi:: not bad. I am thinking to upgrade to DevOps"
