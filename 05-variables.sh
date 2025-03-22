31be2956f5e86fc73ac5c6066d5f7fc5505b7dc6
manishamaidam <maidammanisha.gmail.com>
2025-03-22 13:44:10 +0530

    array

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-devops-tool=("git" "jenkins" "docker" "kubernetes" "terraform" "ansible" "graphana")
+tools=("git" "jenkins" "docker" "kubernetes" "terraform" "ansible" "graphana")
 
 # size of above array is 3.
 # index are 0,1,2
 
 # list always starts with 0.
 
-echo " SCM tool: ${MOVIES[0]}"
-echo " CI/CD tool : ${MOVIES[1]}"
-echo "Few of the top tools of Devops is : ${MOVIES[@]}"
+echo " SCM tool: ${tools[0]}"
+echo " CI/CD tool : ${tools[1]}"
+echo " top tools of Devops is : ${tools[@]}"
