f5f8af15aeeebd086a8841937521207ac7fe1fd9
manishamaidam <maidammanisha.gmail.com>
2025-03-22 13:26:04 +0530

    read option

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,9 @@
 
 ## This program is usecase of reading dynamic variable values from cli 
 echo "Please enter username::"
-read -p USERNAME : #here USERNAME is variable
+read -p USERNAME   #here USERNAME is variable
+
 echo "Please enter password::"
 read -s PASSWORD #s shelds the password from veiw
+
 echo " Hello $USERNAME happy learning "
