8d462593696c82ecbadd31e58314dc1c30f92f3a
manishamaidam <maidammanisha.gmail.com>
2025-03-22 18:37:30 +0530

    conditions using different opetaors

---
+++ 09-conditions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+user=$(id -u) # to exec a command inside shellscript use $
+if ((user -ne 0))
+then
+ echo  "please run the script with root user permissions"
+ exit 1
+fi
+
+ #Note shellscript will proceed further even  it trigger an error to avoid that we have use exit status
+ #exit status 0 is success
+ #exit status other than zero is failure 
+
+read -p "enter the package you want to intall" PACKAGE 
+dnf intall $PACKAGE
+
+if (( $? == 0 ))
+then 
+   echo "installation of $PACKAGE is successful"
+else 
+   echo " installation of $PACKAGE is failed ,please check the error "
+fi
+   
