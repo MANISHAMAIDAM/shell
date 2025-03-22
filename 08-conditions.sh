ae832a318e4a34893d7a3f7d00af4df5aab2386d
manishamaidam <maidammanisha.gmail.com>
2025-03-22 17:47:00 +0530

    conditions using different opetaors

---
+++ 08-conditions.sh
@@ -0,0 +1,21 @@
+## conditions using  following  operators gt,lt,eq,ne,ge,le
+
+echo "enter a number"
+read n
+if (( n -gt 10 ))
+then 
+    echo "$n is greater than 10"
+else 
+    echo "$n is less than 10 "
+
+## conditions using  following  operators >>,<<,==,!=
+
+echo "enter a value"
+read a
+echo "enter a value"
+read b
+if (( a >> b )) 
+then 
+    echo "$a is greater than $b"
+else 
+    echo "$a is less than $b "
